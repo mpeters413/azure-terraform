b6855677fdda2ac3c2c42fecfb05e43e058d21e8
mpeters413 <mpeters413@gmail.com>
2019-04-01 11:56:11 -0400

    update

--- variables.tf
+++ variables.tf
@@ -98,10 +98,10 @@ variable "mysql_hostname" {
 
 ### AKS ###
 
-variable "client_id" {
+variable "arm_client_id" {
   description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster"
 }
 
-variable "client_secret" {
+variable "arm_client_secret" {
   description = "The Client Secret for the Service Principal to use for this Managed Kubernetes Cluster"
 }
