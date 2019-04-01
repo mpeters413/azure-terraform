9f413d171bf29196e06120ed45e0afb45b3f0f92
mpeters413 <mpeters413@gmail.com>
2019-04-01 11:48:43 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -22,19 +22,19 @@ output "App_Server_URL" {
 ### AKS ###
 
 output "id" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.name.id}"
+  value = "${azurerm_kubernetes_cluster.id}"
 }
 
 output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.name.kube_config_raw}"
+  value = "${azurerm_kubernetes_cluster.tf_azure_guide.kube_config_raw}"
 }
 
 output "client_key" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.name.kube_config.0.client_key}"
+  value = "${azurerm_kubernetes_cluster.tf_azure_guide.kube_config.0.client_key}"
 }
 
 output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.name.kube_config.0.client_certificate}"
+  value = "${azurerm_kubernetes_cluster.tf_azure_guide.kube_config.0.client_certificate}"
 }
 
 output "cluster_ca_certificate" {
