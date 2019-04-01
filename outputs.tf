f1d90458abd47a5b6d89896b1509a023ec26d2c1
mpeters413 <mpeters413@gmail.com>
2019-04-01 11:54:33 -0400

    updating output file

--- outputs.tf
+++ outputs.tf
@@ -22,25 +22,25 @@ output "App_Server_URL" {
 ### AKS ###
 
 output "id" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.id}"
+  value = "${azurerm_kubernetes_cluster.mattsAKS.id}"
 }
 
 output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.kube_config_raw}"
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config_raw}"
 }
 
 output "client_key" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.kube_config.0.client_key}"
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_key}"
 }
 
 output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.kube_config.0.client_certificate}"
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_certificate}"
 }
 
 output "cluster_ca_certificate" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.name.kube_config.0.cluster_ca_certificate}"
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.cluster_ca_certificate}"
 }
 
 output "host" {
-  value = "${azurerm_kubernetes_cluster.tf_azure_guide.name.kube_config.0.host}"
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.host}"
 }
