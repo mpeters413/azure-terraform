72ba8d8e802d3480b8708423045dc46d9873fb39
mpeters413 <mpeters413@gmail.com>
2019-04-02 15:16:40 -0400

    updating the AKS Tier

--- files/setup.sh
+++ files/setup.sh
@@ -7,7 +7,7 @@ apt install -y apache2 > /dev/null 2>&1
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Welcome To Staples!</title></head>
+  <head><title>That Was Easy!</title></head>
   <body style="background-image:linear-gradient(black);">
   <center><a href="https://ibb.co/znFqK3n"><img src="https://i.ibb.co/znFqK3n/staples-416x416.jpg" alt="staples-416x416" border="0"></a></center>
   <marquee><h1>Welcome to Staples!</h1></marquee>
