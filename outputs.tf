089f977cc01ee1271366d00066a84cfebc7c264e
mpeters413 <mpeters413@gmail.com>
2019-04-02 18:14:01 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -18,29 +18,3 @@ output "App_Server_URL" {
   value = "http://${azurerm_public_ip.tf-guide-pip.fqdn}"
 }
 
-
-### AKS ###
-
-output "id" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.id}"
-}
-
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config_raw}"
-}
-
-output "client_key" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_key}"
-}
-
-output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_certificate}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.cluster_ca_certificate}"
-}
-
-output "host" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.host}"
-}
