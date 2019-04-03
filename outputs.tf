871d1bcd1f17d1dbec3909f2bce73a8e8510e4df
mpeters413 <mpeters413@gmail.com>
2019-04-03 10:34:41 -0400

    updating with AKS

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,26 @@ output "App_Server_URL" {
   value = "http://${azurerm_public_ip.tf-guide-pip.fqdn}"
 }
 
+################################################
+########## Output Variables AKS ####################
+
+output "id" {
+  value = "${azurerm_kubernetes_cluster.mattsAKS.id}"
+}
+
+output "kube_config" {
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config_raw}"
+}
+
+output "client_key" {
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_key}"
+}
+
+output "client_certificate" {
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_certificate}"
+}
+
+output "host" {
+  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.host}"
+}
+
