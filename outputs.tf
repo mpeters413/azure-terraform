4f611f3a47e5efcbe36401b0fad05eccac469b04
Matthew Peters <mpeters413@gmail.com>
2020-06-23 12:06:23 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,38 +6,4 @@
 # and include any data from the state file. Outputs are sorted alphabetically;
 # use an underscore _ to move things to the bottom. 
 
-output "_instructions" {
-  value = "This output contains plain text. You can add variables too."
-}
-
-output "public_dns" {
-  value = "${azurerm_public_ip.tf-guide-pip.fqdn}"
-}
-
-output "App_Server_URL" {
-  value = "http://${azurerm_public_ip.tf-guide-pip.fqdn}"
-}
-
-################################################
-########## Output Variables AKS ####################
-
-output "id" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.id}"
-}
-
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config_raw}"
-}
-
-output "client_key" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_key}"
-}
-
-output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.client_certificate}"
-}
-
-output "host" {
-  value = "${azurerm_kubernetes_cluster.mattsAKS.kube_config.0.host}"
-}
 
